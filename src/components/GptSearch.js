c1bed8d80645142580ba9cc151aa81ab02a1d52b
mohitchaudhari07 <166442441+mohitchaudhari07@users.noreply.github.com>
2025-09-14 14:13:23 +0530

    Update GptSearch.js

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -161,7 +161,7 @@ const GptSearch = () => {
 
     console.log("TMDB results:", tmdbResults);
 
-    dispatch(addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults.flat(), }));
+    dispatch(addGptMovieResult({ movieNames: gptMovies, movieResults: tmdbResults, }));
   };
 
   return (
