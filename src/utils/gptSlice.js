5a93eb4a9b5234768b920686830078ff2b417e82
mohitchaudhari07 <166442441+mohitchaudhari07@users.noreply.github.com>
2025-09-14 13:57:47 +0530

    Update gptSlice.js

--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -1,49 +1,24 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-
-// const gptSlice = createSlice({
-//     name:'gpt',
-//     initialState: {
-//         showGptSearch: false,
-//         gptMovies: null,
-//         movieNames:null,
-//         movieResults:null,
-
-//     },
-//     reducers:{
-//         toggleGptSearchView : (state)=>{
-//             state.showGptSearch = !state.showGptSearch;
-            
-//         },
-//         addGptMovieResult:(state,action) =>{
-//             const {movieNames, movieResults} = action.payload;
-//             state.movieNames = movieNames;
-//             state.movieResults = movieResults;
-//         },
-//     },
-// });
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const gptSlice = createSlice({
-  name: "gpt",
-  initialState: {
-    showGptSearch: true,
-    gptMovies: null,
-    movieNames: null,
-    movieResults: null,
-  },
-  reducers: {
-    addGptMovieResult: (state, action) => {
-      // Store movie names + results separately
-      state.movieNames = action.payload.movieNames;
-      state.movieResults = action.payload.movieResults;
+    name:'gpt',
+    initialState: {
+        showGptSearch: false,
+        gptMovies: null,
+        movieNames:null,
+        movieResults:null,
+
     },
-    toggleGptSearchView: (state) => {
-      state.showGptSearch = !state.showGptSearch;
+    reducers:{
+        toggleGptSearchView : (state)=>{
+            state.showGptSearch = !state.showGptSearch;
+            
+        },
+        addGptMovieResult:(state,action) =>{
+            const {movieNames, movieResults} = action.payload;
+            state.movieNames = movieNames;
+            state.movieResults = movieResults;
+        },
     },
-  },
 });
-
-export const { addGptMovieResult, toggleGptSearchView } = gptSlice.actions;
-
-export default gptSlice.reducer;
